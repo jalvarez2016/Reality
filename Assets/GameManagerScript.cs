3b9ea24edd1c0deff09482088ae90f48da93cf55
Jason <jasonalvarezcoraiza@gmail.com>
2019-12-23 20:01:50 -0500

    Basic Assets and movement
    Animations and transitions need work

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public Vector3 LeftMovement;   
+    public Vector3 RightMovement;
+    public Vector3 UpMovement;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+    }
+
+    public void playerMovement()
+    {
+        if(Input.GetKeyDown(KeyCode.A)){
+            player.GetComponent<Transform>().position += LeftMovement;
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            player.GetComponent<Transform>().position += RightMovement;
+        }
+        if(Input.GetKeyDown(KeyCode.W)){
+            player.GetComponent<Transform>().position += UpMovement;
+        }
+        
+    }
+}
---
+++ Assets/PlayerAnimScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimScript : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)){
+            animator.SetBool("LefttWalk", true);
+        } else {
+            animator.SetBool("LeftWalk", false);
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            Debug.Log("Right");
+            animator.SetBool("RightWalk", true);
+        } else {
+            animator.SetBool("RightWalk", false);
+        }
+        if(Input.GetKeyDown(KeyCode.W)){
+            animator.SetBool("RightJump", true);
+        } else {
+            animator.SetBool("RightJump", false);
+        }
+    }
+}
